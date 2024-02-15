7d27a3caf4235a22fa8808d394e951c7c37052d0
Valentina <valentina.m.suchkova@gmail.com>
2024-02-15 23:50:56 +0300

    Результат выполнения третьего урока

---
+++ test_lesson_3.py
@@ -0,0 +1,15 @@
+from selene import browser, be, have
+
+def test_positive_search(browser_setting):
+    browser.open('https://google.com')
+    browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
+    browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
+
+def test_negative_search(browser_setting):
+    browser.open('/')
+    browser.element('[name="q"]').should(be.blank).type('qwertyqwerty').press_enter()
+    browser.element('[id="search"]').should(have.text('не найдено'))
+
+
+
+
